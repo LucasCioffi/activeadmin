d2a1b7edc6107a0f5b3b9d094fe6a3741285a41b
LucasCioffi <lucas.cioffi@gmail.com>
2018-09-03 09:08:23 -0400

    Update namespace.rb

--- lib/active_admin/namespace.rb
+++ lib/active_admin/namespace.rb
@@ -166,7 +166,7 @@ module ActiveAdmin
       if current_user_method
         menu.add id: 'current_user', priority: priority, html_options: html_options,
           label: -> { display_name current_active_admin_user },
-          url:   -> "/#{display_name(current_active_admin_user)}",
+          url:   -> {"/#{display_name(current_active_admin_user)}"},
           if:    :current_active_admin_user?
       end
     end
